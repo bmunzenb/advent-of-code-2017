b45f10629c8150a8be84074e50937d46e0aad912
Brian Munzenberger <brian@detroitlabs.com>
2017-12-01 13:27:59 -0500

    solve day 1

---
+++ src/main/java/com/munzenberger/advent/InverseCaptcha.java
@@ -0,0 +1,40 @@
+package com.munzenberger.advent;
+
+public class InverseCaptcha {
+
+	public static String solvePart1(String input) {
+
+		return solve(input, 1);
+	}
+
+	public static String solvePart2(String input) {
+
+		return solve(input, input.length() / 2);
+	}
+
+	private static String solve(String input, int jump) {
+
+		int sum = 0;
+
+		for (int i = 0; i < input.length(); i++) {
+
+			int digit = digitAt(i, input);
+			int compare = digitAt(i+jump, input);
+
+			if (digit == compare) {
+				sum += digit;
+			}
+		}
+
+		return Integer.toString(sum);
+	}
+
+	private static int digitAt(int index, String input) {
+
+		if (index > input.length()-1) {
+			index = index % input.length();
+		}
+
+		return Integer.parseInt(input.substring(index, index+1));
+	}
+}
